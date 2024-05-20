74f659a3518dd9478cf62ca0e4aef08317b185ec
Sangram Das <121278559+Sangram03@users.noreply.github.com>
2024-05-21 04:50:55 +0530

    Update contributorsList.js
    Just me

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+  {
+    id: 426,
+    fullname: "Sangram Das ",
+    username: "https://github.com/Sangram03",
+  },
 ];
